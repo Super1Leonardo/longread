face26337145a119c50728617069ad888bd0c289
Лев <levabaluevvv@gmail.com>
2025-08-09 10:37:38 +0500

    last one

---
+++ Desktop/longread-master/astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import svelte from '@astrojs/svelte';
+
+// https://astro.build/config
+export default defineConfig({
+  devToolbar: { enabled: false },
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [svelte()]
+});
